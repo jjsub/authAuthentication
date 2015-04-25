db8a9fe3543e0e8bd5d2bf6159bef9a1bacdd2de
jjsub <jsamesub@gmail.com>
2015-04-25 11:24:56 -0500

    ui state setUp urlRouteProvider .otherwise for '/' redirect done

---
+++ app/scripts/app.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('authenticationApp').config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+  
+ 
+  
+  $urlRouterProvider.otherwise('/'); /* Handel all the wrong urls request and redirect to home */
+
+  $stateProvider
+
+     .state('main', {
+        url: '/',
+        templateUrl: '/views/main.html'
+
+    })
+
+
+       .state('register', {
+        url: '/register',
+        templateUrl: '/views/register.html'
+
+    });
+  
+}]);
