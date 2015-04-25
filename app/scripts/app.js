f7278738f81ade3a0534ff503ebff1996539f14b
jjsub <jsamesub@gmail.com>
2015-04-25 10:10:13 -0500

    Yo instal ready

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name authenticationApp
+ * @description
+ * # authenticationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('authenticationApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name authenticationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the authenticationApp
+ */
+angular.module('authenticationApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
